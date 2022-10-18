d756c1d85576ffe8a95bfc10e337330d5ce701ce
mrOtoM <mrotomachala@gmail.com>
2022-10-18 13:08:15 +0200

    Pridanie stylovania + reset tlacidlo

--- script.js
+++ script.js
@@ -6,29 +6,33 @@ let score = 20;
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
-const lostGame = function (message) {
-  document.querySelector('.message').textContent = message;
+const winnerStyle = function () {
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+  document.querySelector('header').style.borderBottom = '7px solid #60b347';
+  document.querySelector('.number').style.backgroundColor = '#60b347';
 };
 
-// zobrazenie cisla
-document.querySelector('.message').textContent = secretNumber;
+document.querySelector('.number').textContent = secretNumber; // zobrazene nahodne cislo
 
 // Herna logika
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  console.log(guess);
 
   if (!guess) {
     displayMessage('No numbeeeer!');
   } else if (guess === secretNumber) {
     displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    winnerStyle();
   } else if (guess > secretNumber) {
     if (score > 1) {
       displayMessage('Too high!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      lostGame('We lost the game !');
+      displayMessage('We lost the game !');
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
@@ -36,7 +40,21 @@ document.querySelector('.check').addEventListener('click', function () {
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      lostGame('We lost the game !');
+      displayMessage('We lost the game !');
     }
   }
+});
+
+// Tlacidlo AGAIN
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style = '';
+  document.querySelector('.number').style.width = '';
+  document.querySelector('header').style.borderBottom = '';
+  document.querySelector('.number').style.backgroundColor = '';
 });
