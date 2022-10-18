3e594c9352573b1f2962490f623cc63e924a5210
mrOtoM <mrotomachala@gmail.com>
2022-10-18 13:44:00 +0200

    Refactoring kodu

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1; // Generator nahodneho celeho cisla
 let score = 20;
+let highScore = 0;
 
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+
+const displayMessageWithScore = function (message) {
+  displayMessage(message);
+  score--;
+  document.querySelector('.score').textContent = score;
+};
+
 const winnerStyle = function () {
   document.querySelector('body').style.backgroundColor = '#60b347';
   document.querySelector('.number').style.width = '30rem';
@@ -13,12 +21,9 @@ const winnerStyle = function () {
   document.querySelector('.number').style.backgroundColor = '#60b347';
 };
 
-document.querySelector('.number').textContent = secretNumber; // zobrazene nahodne cislo
-
-// Herna logika
+// Herna logika -----------------------------------------------------------------
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
   if (!guess) {
     displayMessage('No numbeeeer!');
@@ -26,19 +31,20 @@ document.querySelector('.check').addEventListener('click', function () {
     displayMessage('Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     winnerStyle();
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
-      displayMessage('Too high!');
-      score--;
-      document.querySelector('.score').textContent = score;
+      displayMessageWithScore('Too high!');
     } else {
       displayMessage('We lost the game !');
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-      displayMessage('Too low!!');
-      score--;
-      document.querySelector('.score').textContent = score;
+      displayMessageWithScore('Too low!!');
     } else {
       displayMessage('We lost the game !');
     }
