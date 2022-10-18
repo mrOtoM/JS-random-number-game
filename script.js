b6c8997e6105604910aa0d96c6a39e4eff6ed616
mrOtoM <mrotomachala@gmail.com>
2022-10-18 10:20:27 +0200

    Pridanie EventListener na tlacidlo Check

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// document.querySelector('.message').textContent;
+// document.querySelector('.message').textContent = 'Corret number :)';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// let aa = (document.querySelector('.guess').value = 23);
+// console.log(aa);
+// document.querySelector('.guess').value;
+
+// const aaa = function(){
+//     console.log(document.querySelector('.guess').value
+// }
+
+// document.querySelector('.check').addEventListener('click', aaa);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No numbeeeer!';
+  }
+});
