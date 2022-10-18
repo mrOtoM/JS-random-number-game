22225ffd6648fb7758942b7fce006ce101912871
mrOtoM <mrotomachala@gmail.com>
2022-10-18 10:59:09 +0200

    Doplnenie o score > 0

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1; // Generator nahodneho celeho cisla
+let secretNumber = Math.trunc(Math.random() * 20) + 1; // Generator nahodneho celeho cisla
+let score = 20;
 
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+const lostGame = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
-document.querySelector('.message').textContent = secretNumber; // zobrazenie cisla
+// zobrazenie cisla
+document.querySelector('.message').textContent = secretNumber;
 
+// Herna logika
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -17,8 +23,20 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     displayMessage('Correct Number!');
   } else if (guess > secretNumber) {
-    displayMessage('Too high!');
+    if (score > 1) {
+      displayMessage('Too high!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      lostGame('We lost the game !');
+    }
   } else if (guess < secretNumber) {
-    displayMessage('Too low!');
+    if (score > 1) {
+      displayMessage('Too low!!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      lostGame('We lost the game !');
+    }
   }
 });
